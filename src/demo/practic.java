5b6031cfff17a8b9acf1fafb19657d27e401032e
Shree <Shree@Shree-PC>
2022-11-23 01:01:47 +0530

    [aniket]:testNG

--- src/demo/practic.java
+++ src/demo/practic.java
@@ -1,10 +1,27 @@
 package demo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class practic {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Test
+	public void m1 () throws Exception {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shree\\Downloads\\chromedriver.exe\\");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		Thread.sleep(5000);
+		
+		
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("anna.aniket09@gmail.com");
+		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("8484031441");
+	 driver.findElement(By.xpath("//button[@type='submit']")).click();
+	
+	
+	 
 	}
 
 }
